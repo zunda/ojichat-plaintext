d1c004f4c8ca4623fae40887b22b3992ff5d192d
zunda <zundan@gmail.com>
2019-10-18 15:42:59 -1000

    Fix typo

--- js.go
+++ js.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	config := generator.Config{TargetName: "", EmojiNum: 4, PunctiuationLebel: 0}
+	config := generator.Config{TargetName: "", EmojiNum: 4, PunctiuationLevel: 0}
 	text, _ := generator.Start(config)
 	js.Global().Get("document").Call("getElementById", "ojichatbox").Set("innerText", text)
 }
